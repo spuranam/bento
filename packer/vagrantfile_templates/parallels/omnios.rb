73409b2a5e9eae1809883d951d0246874ab40451
Mikhail Zholobov <mzholobov@parallels.com>
2014-10-01 12:47:01 +0400

    Fixed Vagrantfile templates for Parallels

--- packer/vagrantfile_templates/parallels/omnios.rb
+++ packer/vagrantfile_templates/parallels/omnios.rb
@@ -1,11 +1,9 @@
 # -*- mode: ruby -*-
 # # vi: set ft=ruby :
 
-Vagrant.require_version ">= 1.6.3"
+Vagrant.require_version ">= 1.1.0"
 
 Vagrant.configure("2") do |config|
-  config.ssh.shell = "sh"
-
   # Disable the base shared folder, Guest Tools are unavailable.
   config.vm.synced_folder ".", "/vagrant", disabled: true
 
