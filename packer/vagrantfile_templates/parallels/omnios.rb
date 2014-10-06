5bd6a71c7e57e16cfc09d831872ba794cc46278a
Rickard von Essen <rickard.von.essen@gmail.com>
2014-10-06 10:10:53 +0200

    Corrected typos in FreeBSD and OmniOS Vagrant template for Parallels

--- packer/vagrantfile_templates/parallels/omnios.rb
+++ packer/vagrantfile_templates/parallels/omnios.rb
@@ -7,9 +7,9 @@ Vagrant.configure("2") do |config|
   # Disable the base shared folder, Guest Tools are unavailable.
   config.vm.synced_folder ".", "/vagrant", disabled: true
 
-  config.vm.provider :parallels do |p|
+  config.vm.provider :parallels do |prl|
     # Guest Tools are unavailable.
     prl.check_guest_tools = false
-    prl.functional_psf    = false    
+    prl.functional_psf    = false
   end
 end
